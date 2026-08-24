99c11da8d4b89d25d2866f2e88e500c183e1bd9e
technovis <technovisone@gmail.com>
2026-08-24 03:55:12 +0100

    Suppression des commentaires dans models.py

--- comptes/models.py
+++ comptes/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-# comptes/models.py
 from django.contrib.auth.models import AbstractUser
-
 class Utilisateur(AbstractUser):
-    """Classe mère — remplace le User par défaut de Django."""
 
     ROLE_PATIENT = 'patient'
     ROLE_MEDECIN = 'medecin'
@@ -21,7 +18,6 @@ class Utilisateur(AbstractUser):
         return f"{self.first_name} {self.last_name} ({self.role})"
 
 class Patient(Utilisateur):
-    """Hérite d'Utilisateur (héritage multi-tables Django)."""
     date_naissance = models.DateField()
     num_tel = models.CharField(max_length=20)
     adresse = models.CharField(max_length=255)
@@ -31,13 +27,9 @@ class Patient(Utilisateur):
         verbose_name_plural = "Patients"
 
 class Medecin(Utilisateur):
-    """Hérite d'Utilisateur (héritage multi-tables Django)."""
     specialite = models.CharField(max_length=100)
     num_rpps = models.CharField(max_length=20, unique=True)
     planning = models.TextField(blank=True)
-
     class Meta:
         verbose_name = "Médecin"
         verbose_name_plural = "Médecins"
-
-# Create your models here.
